c192686a9c8d07a2225863ed8f5f90cb31a970d4
hmbyun3212 <hmbyun3212@gmail.com>
2026-03-17 10:17:06 +0900

    변경

--- src/자동차만들기/Car.java
+++ src/자동차만들기/Car.java
@@ -1,13 +1,16 @@
 package 자동차만들기;
+// abstract : 추상화
+// 역할: 모든 자동차의 공통적인 특징(속도, 연비 등)을 한곳에 모아두는 중심축 역할
 
 public abstract class Car {
 
+    //protected: 보안을 위해 외부(다른 패키지)에서는 감추되, 상속받은 자식 클래스들은 자유롭게 사용할 수 있게 허용하는 접근 제한자
     protected int speed;
     protected double mileage;
     protected int tank;
     protected int seat;
     protected String name;
-
+    //this : 외부에서 들어온 값(오른쪽)과 내 주머니의 변수(왼쪽)를 구분하기 위해 사용
     public Car(int speed, double mileage, int tank, int seat, String name) {
         this.speed = speed;
         this.mileage = mileage;
